38317e17e7fc1ee70f489629c5902418e2a92da2
JM <98504627+Paqong@users.noreply.github.com>
2023-03-14 21:25:18 +0800

    Added the Product List Component ehe
    Also set up the services needed and applied dynamic content to it

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,15 +3,21 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsURL = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map( response => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsURL).map(response => <Product[]>response.json())
   }
 }
